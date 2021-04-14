f67a80e479bb6974e251609e7e8576bf03b5b29f
alexhstrickland <alexhstrickland@gmail.com>
2021-04-14 19:15:28 -0400

    adding db

---
+++ public/db.js
@@ -0,0 +1,24 @@
+let database;
+
+const request = indexedDB.open("budget", 1);
+
+request.upgradeneeded = function(e) {
+    const database = e.target.result;
+    database.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onerror = function(e) {
+    console.log("Error");
+};
+
+request.onsuccess = function(e) {
+    database = e.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["transaction", "readwrite"]);
+}
