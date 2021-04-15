94f47b159d7145519756c036f03c2211930d364e
alexhstrickland <alexhstrickland@gmail.com>
2021-04-14 20:07:58 -0400

    completed db

--- public/db.js
+++ public/db.js
@@ -21,4 +21,34 @@ request.onsuccess = function(e) {
 
 function checkDatabase() {
     const transaction = db.transaction(["transaction"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.success = function() {
+        if(getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["transaction"], "readwrite");
+                const store = transaction.objectStore
+                store.clear();
+            });
+        }
+    }
+
+    function saveRecord(record) {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.add(record);
+    }
+
 }
+
+window.addEventListener("online", checkDatabase);
